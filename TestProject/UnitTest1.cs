dd70864c84c342896f2be1c4540404ff2dbe160a
Sorawit Chuanchoei <65030255@kmitl.ac.th>
2023-10-18 10:53:01 +0700

    Update

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -10,5 +10,15 @@ namespace TestProject
         {
             Assert.IsTrue(Enum.GetNames(typeof(Project.Members)).Length == 4);
         }
+        [TestMethod]
+        public void TestProjectMemberIds()
+        {
+            Assert.IsTrue((int)Project.Members.Tiew == 1);
+            Assert.IsTrue((int)Project.Members.Ping == 2);
+            Assert.IsTrue((int)Project.Members.Arm == 3);
+            Assert.IsTrue((int)Project.Members.Fulk == 4);
+        }
     }
+
+    
 }
